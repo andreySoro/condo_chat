637c2e2a6e30793d04ae27d70880e989da077c53
Andrey <andrey@wewearmanyhats.com>
2022-08-03 15:57:09 -0600

    add auth check to refreshToken endpoint

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const requireAuth = require("../../middleware/auth.check");
 const forgotPasswordController = require("../../controllers/auth/forgotPassword.controller");
 const refreshTokenController = require("../../controllers/auth/refreshToken.controller");
 const signInController = require("../../controllers/auth/signIn.controller");
 const signUpController = require("../../controllers/auth/signUp.controller");
 
 router.post("/forgotPassword", forgotPasswordController);
-router.post("/refreshToken", refreshTokenController);
+router.post("/refreshToken", requireAuth, refreshTokenController);
 router.post("/signIn", signInController);
 router.post("/signUp", signUpController);
 
