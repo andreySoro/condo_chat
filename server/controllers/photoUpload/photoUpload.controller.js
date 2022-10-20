0199b8379eb3b205882f9a625be42b128b79ebfd
Andrey <sorokin.andry1990@gmail.com>
2022-10-20 13:36:11 -0600

    Merge branch 'main' of bitbucket.org:many-hats/condochatapp-backend into CDA-8

--- server/controllers/photoUpload/photoUpload.controller.js
+++ server/controllers/photoUpload/photoUpload.controller.js
@@ -12,13 +12,58 @@ const photoUpload = async (req, res) => {
     folder[req.body.folder],
     token,
   );
+
   if (!uploadedImages || uploadedImages.length === 0) {
     return res.status(400).send({
       data: [],
       error: { message: "Photo is not provided or nothing was uploaded" },
     });
   }
+
   return res.status(200).json({ uploadedImages });
 };
 
-module.exports = photoUpload;
+const profilePhotoUpload = async (req, res) => {
+  // For old approach, likely no longer need.
+  // const UserId = req.UserId;
+  if (req.files.length !== 1) {
+    return res.status(401).send({
+      data: [],
+      error: {
+        message: "Please provide only a single image file."
+      }
+    })
+  }
+
+  // Check that the extension is either a jpg, jpeg, or png.
+  const extension = req.body.photos[0].originalname.split('.')[1];
+
+  if (extension !== 'jpg'
+    && extension !== 'jpeg'
+    && extension !== 'png') {
+    return res.status(401).send({
+      data: [],
+      error: { message: "Please provide a file with one of the following extensions: [jpg, jpeg, png]" }
+    })
+  }
+
+  const uploadedImages = await getUploadedImagesUrl(
+    req?.body?.photos,
+    `profile`,
+    1 // You may only upload a single file for this endpoint.
+  );
+
+  if (!uploadedImages || uploadedImages.length === 0) {
+    return res.status(400).send({
+      data: [],
+      error: { message: "Photo is not provided or nothing was uploaded" },
+    });
+  }
+
+  return res.status(200).json({ uploadedImages });
+}
+
+module.exports = {
+  photoUpload,
+  profilePhotoUpload,
+};
