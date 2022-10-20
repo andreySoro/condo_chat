deb45b4c789379bcf8f14bd354911d9ef16b2840
Robert <robert@wewearmanyhats.com>
2022-10-20 16:15:13 -0300

    Added profile image uri field to user in Models as well.

--- server/models/User.js
+++ server/models/User.js
@@ -8,6 +8,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, unique: true },
   unitNumber: { type: String },
+  profileImgUri: { type: String },
   address: { type: Number },
   reputation: { type: Number, default: 0 },
   fcmTokens: { type: [String], default:[] },
