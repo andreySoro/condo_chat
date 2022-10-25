9c6cdf9d1b8555a37b2f8209b2ecda4af45428a0
Andrey <sorokin.andry1990@gmail.com>
2022-10-25 13:54:05 -0600

    changed to split and few logs

--- server/utils/extractUserIdFromToken.js
+++ server/utils/extractUserIdFromToken.js
@@ -4,7 +4,7 @@ const admin = require("firebase-admin");
 
 const extractUserIdFromToken = async (auth) => {
   console.log("auth check func =", auth);
-  const userToken = auth;
+  const userToken = auth.split(" ")[1] || auth;
   console.log("user toke auth func", userToken);
   const decodedToken = jwt.decode(userToken);
   console.log("decoded token", decodedToken);
@@ -16,7 +16,9 @@ const extractUserIdFromToken = async (auth) => {
     //   `https://oauth2.googleapis.com/tokeninfo?id_token=${userToken}`
     // );
     // const { email } = data;
+    console.log("EMAIL DEOCDED", decodedToken.email);
     const { uid } = await admin.auth().getUserByEmail(decodedToken.email);
+    console.log("uid", uid);
     return uid;
   }
 };
