ced3cc33b33415c1e1264025b80880da581da8af
Robert <robert@wewearmanyhats.com>
2022-11-02 16:05:17 -0300

    Added catch for all uncaught errors in photoUpload controller.

--- server/controllers/photoUpload/photoUpload.controller.js
+++ server/controllers/photoUpload/photoUpload.controller.js
@@ -4,66 +4,71 @@ const User = require("../../models/User");
 const admin = require("firebase-admin");
 
 const photoUpload = async (req, res) => {
-  const UserId = req.UserId;
+  try {
+    const UserId = req.UserId;
 
-  if (!req.body.photos || (!req.body.folder && req.body.folder !== 0)) {
-    return res.status(400).send("Photo or folder is not provided");
-  }
-  const folder = ["profile", "posts", "comments"];
+    if (!req.body.photos || (!req.body.folder && req.body.folder !== 0)) {
+      return res.status(400).send("Photo or folder is not provided");
+    }
+    const folder = ["profile", "posts", "comments"];
 
-  const uploadedImages = await getUploadedImagesUrl(
-    req.body.photos,
-    folder[req.body.folder],
-    UserId
-  );
+    const uploadedImages = await getUploadedImagesUrl(
+      req.body.photos,
+      folder[req.body.folder],
+      UserId
+    );
 
-  if (!uploadedImages || uploadedImages.length === 0) {
-    return res.status(400).send({
-      data: [],
-      error: { message: "Photo is not provided or nothing was uploaded" },
-    });
-  }
-
-  // Upon successful upload complete any bookkeeping tasks for the user
-  //  regarding the upload.
-  if (req.body.folder === 0) {
-    // If it was a profile image
-    if (req.body.photos.length !== 1) {
+    if (!uploadedImages || uploadedImages.length === 0) {
       return res.status(400).send({
         data: [],
-        error: { message: "Please provide only 1 photo as a profile upload" },
+        error: { message: "Photo is not provided or nothing was uploaded" },
       });
     }
 
-    const user = await User.findOne({ id: UserId });
-    const oldUri = user.profileImgUri;
+    // Upon successful upload complete any bookkeeping tasks for the user
+    //  regarding the upload.
+    if (req.body.folder === 0) {
+      // If it was a profile image
+      if (req.body.photos.length !== 1) {
+        return res.status(400).send({
+          data: [],
+          error: { message: "Please provide only 1 photo as a profile upload" },
+        });
+      }
 
-    if (oldUri) {
-      // Find and delete the old image from our bucket.
-      const bucket = admin.storage().bucket();
-      const oldFile = bucket.file(oldUri);
+      const user = await User.findOne({ id: UserId });
+      const oldUri = user.profileImgUri;
 
-      await oldFile.delete();
+      if (oldUri) {
+        // Find and delete the old image from our bucket.
+        const bucket = admin.storage().bucket();
+        const oldFile = bucket.file(oldUri);
+
+        await oldFile.delete();
+      }
+
+      // Set the users profileImgUri
+      //  to the new Uri.
+      console.log(
+        `Attempting to set User profileImgUri to: ${uploadedImages[0]}`
+      );
+      const userUpdate = await User.findOneAndUpdate(
+        { id: UserId },
+        {
+          $set: {
+            profileImgUri: uploadedImages[0],
+          },
+        },
+        { new: true }
+      );
+      // Catch error and handle before committing.
     }
 
-    // Set the users profileImgUri
-    //  to the new Uri.
-    console.log(
-      `Attempting to set User profileImgUri to: ${uploadedImages[0]}`
-    );
-    const userUpdate = await User.findOneAndUpdate(
-      { id: UserId },
-      {
-        $set: {
-          profileImgUri: uploadedImages[0],
-        },
-      },
-      { new: true }
-    );
-    // Catch error and handle before committing.
+    return res.status(200).json({ uploadedImages });
+  } catch (e) {
+    console.error('Issue with Image Upload');
+    console.error(e);
   }
-
-  return res.status(200).json({ uploadedImages });
 };
 
 const profilePhotoUpload = async (req, res) => {
